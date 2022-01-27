3c89cddf0720ba6e114c5b5ee7dc60f57b8e0293
Gautam Borkar <gborkar@us.ibm.com>
2022-01-27 12:14:58 -0500

    Added autowiring on publish create account.

--- payment-service/src/main/java/org/github/gborkar/saga/chreography/paymentservice/kafka/PublishCreateAccount.java
+++ payment-service/src/main/java/org/github/gborkar/saga/chreography/paymentservice/kafka/PublishCreateAccount.java
@@ -1,10 +1,20 @@
 package org.github.gborkar.saga.chreography.paymentservice.kafka;
 
+import org.github.gborkar.saga.chreography.paymentservice.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
 
 @SuppressWarnings("deprecation")
 @EnableBinding(Source.class)
 public class PublishCreateAccount {
+    @Autowired
+    private MessageChannel output;
+
+    public void raiseCreateAccount(Account account) {
+        output.send(MessageBuilder.withPayload(account).build());
+    }
 
 }
--- payment-service/src/main/java/org/github/gborkar/saga/chreography/paymentservice/rest/controller/AccountService.java
+++ payment-service/src/main/java/org/github/gborkar/saga/chreography/paymentservice/rest/controller/AccountService.java
@@ -4,17 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.github.gborkar.saga.chreography.paymentservice.kafka.PublishCreateAccount;
 import org.github.gborkar.saga.chreography.paymentservice.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.integration.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class AccountService {
     @Autowired
-    private Source source;
+    PublishCreateAccount publishCreateAccount;
 
     private static List<Account> accountList = new ArrayList<>(
             Arrays.asList(new Account(1L, 100), new Account(2L, 200)));
@@ -28,7 +27,7 @@ public class AccountService {
     }
 
     public Long addAccount(@RequestBody Account account) {
-        source.output().send(MessageBuilder.withPayload(account).build());
+        publishCreateAccount.raiseCreateAccount(account);
         return account.getAccountId();
     }
 }
