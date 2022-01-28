a4d01e14164286284d24a77ad3f8ed89507d50cb
Gautam Borkar <gborkar@us.ibm.com>
2022-01-27 19:18:58 -0500

    Added kafka consumer

--- account-service/src/main/java/org/github/gborkar/saga/chreography/accountservice/kafka/AccountServiceProcessor.java
+++ account-service/src/main/java/org/github/gborkar/saga/chreography/accountservice/kafka/AccountServiceProcessor.java
@@ -1,5 +1,19 @@
 package org.github.gborkar.saga.chreography.accountservice.kafka;
 
-public class AccountServiceProcessor {
+import java.util.function.Consumer;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.stereotype.Component;
+
+@Component(value = "process")
+public class AccountServiceProcessor
+        implements Consumer<KStream<Object, String>> {
+
+    @Override
+    public void accept(KStream<Object, String> input) {
+        input.foreach((key, value) -> {
+            System.out.println("Key: " + key + " Value: " + value);
+        });
+    }
 
 }
